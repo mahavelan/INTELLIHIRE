3e2cf1e82c8fcbbadbdd7dc9ef1e9ca35606ba41
mahavelan <mahamahalakshmi1112@gmail.com>
2025-07-08 18:27:16 +0530

    Delete app (14).py

--- app (14).py
+++
@@ -1,8 +0,0 @@
-
-import streamlit as st
-
-st.set_page_config(page_title="IntelliHire App", layout="wide")
-
-st.title("Welcome to IntelliHire")
-
-st.write("This is the base layout for the IntelliHire platform. Each module like login, profile, ATS, AI Training, and Chatbot will be integrated here.")
